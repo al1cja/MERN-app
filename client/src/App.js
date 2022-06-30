fd9a8ee2b9c8866d03eb111ac5436c0f0b7e7abc
amarkowska <amarkowska@microstrategy.com>
2022-06-30 19:58:42 +0200

    Add User input

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,6 @@ import AdminPanel from "./components/admin-panel/adminPanel";
 import AddUser from "./components/add-user/addUser"
 import EditUser from "./components/edit-user/editUser";
 
-import Login from "./components/login/login";
-
 import './app.css';
 
 const App = () => {
@@ -15,7 +13,6 @@ const App = () => {
         <Route path="/" element={<AdminPanel/>} />
         <Route path="add-user" element={<AddUser />} />
         <Route path="edit-user/:userId" element={<EditUser />} />
-        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
