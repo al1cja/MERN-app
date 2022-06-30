d8f1a0308c5d741a37e0b53cad7c017853a4cc99
amarkowska <amarkowska@microstrategy.com>
2022-06-30 20:05:14 +0200

    Add Cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 
 const connectDB = require('./database/connection');
 const router = require("./routes/router")
@@ -6,9 +7,10 @@ const router = require("./routes/router")
 const app = express();
 const port = 3030;
 
-
 connectDB()
+
 app.use(express.json());
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Server Started at ${port}`)
